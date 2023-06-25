52facee4b0412186dc5af486b21196f09531ab18
m1cha1s <msulej04@gmail.com>
2023-06-25 21:26:45 +0200

    Minor changes.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.git_files, {})
--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,1 +1,32 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ef", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>sv", function () vim.cmd("vsplit") end)
+vim.keymap.set("n", "<leader>sh", function () vim.cmd("split") end)
+
+vim.keymap.set("n", "<leader>cb", function () 
+    ft = vim.bo.filetype
+    if ft == "c" or ft == "cpp" then
+        vim.cmd("!make build") 
+    elseif ft == "rust" then
+        vim.cmd("!cargo build")
+    end
+end)
+vim.keymap.set("n", "<leader>cc", function () 
+    ft = vim.bo.filetype
+    if ft == "c" or ft == "cpp" then
+        vim.cmd("!make clean") 
+    elseif ft == "rust" then
+        vim.cmd("!cargo clean")
+    end
+end)
+vim.keymap.set("n", "<leader>cr", function () 
+    ft = vim.bo.filetype
+    if ft == "c" or ft == "cpp" then
+        vim.cmd("!make run") 
+    elseif ft == "rust" then
+        vim.cmd("!cargo run")
+    elseif ft == "python" then
+        vim.cmd("!python ./main.py")
+    end
+end)
+
